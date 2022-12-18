c6969754f07becac6bb76ad5681c461c7d282288
panpan <1035972704@qq.com>
2022-12-18 14:00:24 +0800

    ok please change.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off', //为了解决格式化和es规范的冲突
+    indent: 0,
+    'space-before-function-paren': 0
   }
 }
