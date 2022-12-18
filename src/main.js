c9303ebe6c69af0902c45cd53e7480e317616f5a
panpan <1035972704@qq.com>
2022-12-18 23:06:37 +0800

    fix: add

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import '@/styles/index.scss'
-
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+// 为什么是icons这下面的
+import SvgIcon from '@/icons'
+const app = createApp(App)
+SvgIcon(app)
+app.use(store).use(router).use(ElementPlus).mount('#app')
